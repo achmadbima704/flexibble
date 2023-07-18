5ae49574bc421b514ccf2a10a57d2b7f1d6ff4aa
achmadbima704 <achmadbima704@gmail.com>
2023-07-18 18:42:19 +0700

    OK

---
+++ lib/session.ts
@@ -0,0 +1,76 @@
+import {getServerSession} from "next-auth/next";
+import {NextAuthOptions, User} from "next-auth";
+import {AdapterUser} from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
+import jsonwebtoken from 'jsonwebtoken';
+import {JWT} from "next-auth/jwt";
+import {SessionInterface, UserProfile} from "@/common.types";
+import {createUser, getUser} from "./actions";
+
+export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    })
+  ],
+  jwt: {
+    encode: ({ secret, token }) => {
+      return jsonwebtoken.sign({
+        ...token,
+        iss: 'grafbase',
+        exp: Math.floor(Date.now() / 1000) + 60 * 60
+      }, secret)
+    },
+    decode: async ({ secret, token }) => {
+      return jsonwebtoken.verify(token!, secret) as JWT
+    }
+  },
+  theme: {
+    colorScheme: 'auto',
+  },
+  callbacks: {
+    async session({ session, token, user }) {
+      const email =  session?.user?.email as string
+
+      try {
+        const data = await getUser(email) as { user?: UserProfile }
+
+        return {
+          ...session,
+          user: {
+            ...session.user,
+            ...data.user
+          }
+        }
+      } catch (error) {
+        console.log('Error retrieving user data')
+        return session
+      }
+    },
+    async signIn({ user }: { user: User | AdapterUser }) {
+      try {
+        // Get user if they exist
+        const userExist = await getUser(user?.email as string) as { user?: UserProfile };
+
+        // if they don't exist create them
+        if (!userExist) {
+          await createUser(
+            user.name as string,
+            user.email as string,
+            user.image as string
+          )
+        }
+
+        return true
+      } catch (error: any) {
+        return false
+      }
+    }
+  }
+}
+
+export async function getCurrentUser() {
+  return await getServerSession(authOptions) as SessionInterface
+}
